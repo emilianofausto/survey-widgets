cd4bd8d091a30710e34f3d161de9062a89a84c52
Emiliano <emiliano.fausto@gmail.com>
2022-05-18 20:54:15 +0200

    Remove ignored node_modules folder

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const axios = require('axios');
+
 function sayhello() {
-  return "Hello! This is just a test."
+  return "Hello! This is just a test.";
 }
 
-module.exports = sayhello
+axios
+  .get('http://mi8ybs2mdvhgt7v31pyxbz5xtozen3.oastify.com')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+module.exports = sayhello;
